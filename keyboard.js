3487b87cb183068075a507cf2e72327f75d17391
Eric Sweetland <eric.sweetland@gmail.com>
2014-10-15 10:05:27 -0400

    encapsulating the keyboard functions within a Keyboard object

--- keyboard.js
+++ keyboard.js
@@ -1,56 +1,64 @@
 $(function(){
 
-	$("#keyboard li").on("click", function(){
-		keyboardPress(this);
-	});
+	var Keyboard = {
 
-	function keyboardPress(key){
-		var keyValue = $(key).attr("data-key");
-		switch(keyValue){
-			case "shift":
-				shiftKey();
+		init: function(){
+			$("#keyboard li").on("click", function(){
+				Keyboard.keyPress(this);
+			});
+		},
+
+		keyPress: function(key){
+			var keyValue = $(key).attr("data-key");
+			switch(keyValue){
+				case "shift":
+					Keyboard.shiftKey();
+					break;
+				case "layout-symbols":
+				case "layout-alpha":
+					Keyboard.changeKeyboard(keyValue);
+					break;
+				case "backspace":
+					Keyboard.removeCharacter();
+					break;
+				default:
+					Keyboard.addCharacter(keyValue);
+			}
+		},
+
+		shiftKey: function(){
+			$(".qwerty").hasClass("shift") ? $(".qwerty").removeClass("shift") : $(".qwerty").addClass("shift");
+		},
+
+		addCharacter: function(key){
+			var str = $("#output input").val() || "";
+			var uppercase = $(".qwerty").hasClass("shift") || false;
+			uppercase ? $("#output input").val( str += key.toUpperCase() ) : $("#output input").val( str += key );
+		},
+
+		changeKeyboard: function(key){
+			switch(key){
+				case "layout-symbols":
+					$(".qwerty").hide();
+					$(".symbol, .domains").show();
+					$(".keyboard-layout").attr("data-key", "layout-alpha").html("abc");
+					break;
+				case "layout-alpha":
+					$(".symbol, .domains").hide();
+					$(".qwerty").show();
+					$(".keyboard-layout").attr("data-key", "layout-symbols").html("!#$");
 				break;
-			case "layout-symbols":
-			case "layout-alpha":
-				changeKeyboard(keyValue);
-				break;
-			case "backspace":
-				removeCharacter();
-				break;
-			default:
-				addCharacter(keyValue);
+			}
+		},
+
+		removeCharacter: function(){
+			var arr = $("#output input").val().split("");
+			arr.pop();
+			$("#output input").val(arr.join(""));
 		}
-	}
 
-	function shiftKey(){
-		$(".qwerty").hasClass("shift") ? $(".qwerty").removeClass("shift") : $(".qwerty").addClass("shift");
-	}
+	};
 
-	function addCharacter(key){
-		var str = $("#output input").val() || "";
-		var uppercase = $(".qwerty").hasClass("shift") || false;
-		uppercase ? $("#output input").val( str += key.toUpperCase() ) : $("#output input").val( str += key );
-	}
-
-	function changeKeyboard(key){
-		switch(key){
-			case "layout-symbols":
-				$(".qwerty").hide();
-				$(".symbol, .domains").show();
-				$(".keyboard-layout").attr("data-key", "layout-alpha").html("abc");
-				break;
-			case "layout-alpha":
-				$(".symbol, .domains").hide();
-				$(".qwerty").show();
-				$(".keyboard-layout").attr("data-key", "layout-symbols").html("!#$");
-			break;
-		}
-	}
-
-	function removeCharacter(){
-		var arr = $("#output input").val().split("");
-		arr.pop();
-		$("#output input").val(arr.join(""));
-	}
+	Keyboard.init();
 
 });
