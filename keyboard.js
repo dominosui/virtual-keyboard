8c5ed8f1700b9b447cfa3e293163f04f78677b23
Eric Sweetland <eric.sweetland@gmail.com>
2014-10-17 09:37:10 -0400

    This allows the user to navigate the keyboard via the arrow keys.  Thanks mong8se for the help

--- keyboard.js
+++ keyboard.js
@@ -2,10 +2,34 @@ $(function(){
 
 	var Keyboard = {
 
+		defaultKey: "g",
+		activeKey: "",
+
 		init: function(){
 			$("#keyboard li").on("click", function(){
 				Keyboard.keyPress(this);
 			});
+			$("body").focus();
+			$("body").keydown(function(evt){
+				switch(evt.keyCode){
+					case 37:
+						Keyboard.nav.Left();
+						break;
+					case 38:
+						Keyboard.nav.Up();
+						break;
+					case 39:
+						Keyboard.nav.Right();
+						break;
+					case 40:
+						Keyboard.nav.Down();
+						break;
+					case 13:
+						Keyboard.keyPress(Keyboard.activeKey);
+				}
+			});
+			//$("#keyboard li[data-key='g']").addClass("focus");
+			Keyboard.resetFocus($("#keyboard li[data-key='"+ Keyboard.defaultKey +"']"));
 		},
 
 		keyPress: function(key){
@@ -24,6 +48,7 @@ $(function(){
 				default:
 					Keyboard.addCharacter(keyValue);
 			}
+			Keyboard.resetFocus(key);
 		},
 
 		shiftKey: function(){
@@ -55,6 +80,90 @@ $(function(){
 			var arr = $("#output input").val().split("");
 			arr.pop();
 			$("#output input").val(arr.join(""));
+		},
+
+		resetFocus: function(key){
+			var keyData = $(key).attr("data-key")
+			$("#keyboard li").removeClass("focus");
+			$("#keyboard li[data-key='" + keyData + "']").addClass("focus");
+			Keyboard.activeKey = key;
+		},
+
+		nav : {
+			Right: function(){
+				console.log("Right");
+				var parentElem = $(Keyboard.activeKey).parent();
+				var siblings = $("li", parentElem);
+				var wrap = false;
+				for(var i=0, iL=siblings.length; i<iL; i++){
+					if( $(siblings[i]).hasClass("focus") ){
+						if( (i+1) >= siblings.length ){
+							wrap = true;
+						}
+						break;
+					}
+				}
+				wrap ? Keyboard.resetFocus($(siblings).eq(0)) : Keyboard.resetFocus($(Keyboard.activeKey).next());
+			},
+			Left: function(){
+				console.log("Left");
+				var parentElem = $(Keyboard.activeKey).parent();
+				var siblings = $("li", parentElem);
+				var wrap = false;
+				for(var i=0, iL=siblings.length; i<iL; i++){
+					if( $(siblings[i]).hasClass("focus") ){
+						if( i == 0 ){
+							wrap = true;
+						}
+						break;
+					}
+				}
+				wrap ? Keyboard.resetFocus($(siblings).eq(siblings.length - 1)) : Keyboard.resetFocus($(Keyboard.activeKey).prev());
+			},
+			Up: function(){
+				console.log("Up");
+				var parentElem = $(Keyboard.activeKey).parent().parent();
+				if( !$(parentElem).hasClass("numeric") ){
+					var newElement = document.elementFromPoint( $(Keyboard.activeKey).offset().left + $(Keyboard.activeKey).outerWidth()/2, $(Keyboard.activeKey).offset().top - $(Keyboard.activeKey).outerHeight()*0.5);
+					if(newElement.tagName.toUpperCase() != "LI") {
+						newElement = document.elementFromPoint( $(Keyboard.activeKey).offset().left + $(Keyboard.activeKey).outerWidth()/2.2, $(Keyboard.activeKey).offset().top - $(Keyboard.activeKey).outerHeight()*0.5);
+					}
+					Keyboard.resetFocus(newElement);
+				}
+			},
+			Down: function(){
+				console.log("Down");
+				var parentElem = $(Keyboard.activeKey).parent().parent();
+				if( !$(parentElem).hasClass("bottom-nav") ){
+					var newElement = document.elementFromPoint( $(Keyboard.activeKey).offset().left + $(Keyboard.activeKey).outerWidth()/2, $(Keyboard.activeKey).offset().top + $(Keyboard.activeKey).outerHeight()*1.5);
+					if(newElement.tagName.toUpperCase() != "LI") {
+						newElement = document.elementFromPoint( $(Keyboard.activeKey).offset().left + $(Keyboard.activeKey).outerWidth()/2.2, $(Keyboard.activeKey).offset().top + $(Keyboard.activeKey).outerHeight()*1.5);
+					}
+					Keyboard.resetFocus(newElement);
+				}
+			},
+			getPreviousVisibleElement: function(elem){
+				var tempElem = isVisible($(elem).prev());
+				function isVisible(x){
+					if( $(x).is(":visible") ){
+						return $(x);
+					} else {
+						return isVisible($(x).prev());
+					}
+				}
+				return tempElem;
+			},
+			getNextVisibleElement: function(elem){
+				var tempElem = isVisible($(elem).next());
+				function isVisible(x){
+					if( $(x).is(":visible") ){
+						return $(x);
+					} else {
+						return isVisible($(x).next());
+					}
+				}
+				return tempElem;
+			}
 		}
 
 	};
